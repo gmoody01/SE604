150352d534071b5abae88a256275afe81f85ebe6
jen-wasilenko <52016485+jen-wasilenko@users.noreply.github.com>
2019-06-26 19:38:07 -0500

    Update importcsv.py

--- importcsv.py
+++ importcsv.py
@@ -7,6 +7,7 @@ with open('DJIA.csv', 'r') as cvs_file:
    for line in cvs_reader:
         print(line)
 
+# test test test to see if jen can edit gmoody01's file test test test
 
 #    column =
 #    sqrts = [math.sqrt(x) for x in column]
